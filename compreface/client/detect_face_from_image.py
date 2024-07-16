c55a1250bd48d392ed931e2e50b8981372a6b6dd
starkwpgots <starkwpgots@outlook.com>
2024-07-16 13:36:22 +0530

    Update detect_face_from_image.py

--- compreface/client/detect_face_from_image.py
+++ compreface/client/detect_face_from_image.py
@@ -30,7 +30,7 @@ class DetectFaceFromImageClient(ClientRequest):
         super().__init__()
         self.client_url: str = DETECTION_API
         self.api_key: str = api_key
-        self.url: str = domain + ':' + port + self.client_url
+        self.url: str = domain + self.client_url
 
     def get(self):
         pass
